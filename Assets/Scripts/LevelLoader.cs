167a11540ae5b1ffe99282eaa9cc12d12b0d591d
EricLam3141 <143209963+EricLam3141@users.noreply.github.com>
2024-08-05 17:16:37 -0400

    Initial Commit
    Initial Commit

---
+++ Assets/Scripts/LevelLoader.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Checks : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "LevelExit")
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+        if (other.tag == "GameReset")
+        {
+            SceneManager.LoadScene(0);
+        }
+        if (other.tag == "GameExit")
+        {
+            SceneManager.LoadScene(6);
+        }
+        if (other.tag == "DeathScene5")
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+    }
+
+}
